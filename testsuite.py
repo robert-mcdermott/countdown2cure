1f7f31058c8950cdad044ca1bf3a333bfd17b46a
Robert McDermott <robert-mcdermott@users.noreply.github.com>
2017-02-23 16:00:51 -0800

    Update testsuite.py

--- testsuite.py
+++ testsuite.py
@@ -3,12 +3,12 @@ import sys
 
 def test(target):
     if target == 'pass':
-        for x in range(1,10):
+        for x in range(1,15):
             print("Test \'%s\': PASS" % x)
         sys.exit(0)
 
     if target == 'fail':
-        for x in range(1,10):
+        for x in range(1,15):
             print("Test \'%s\': FAIL" % x)
         sys.exit(99)
 
